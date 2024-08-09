e1dcfd4552e1e47bcde87155bf4eace1ed0da41a
Lifo123 <alberto_p-@hotmail.com>
2024-08-09 16:51:04 -0500

    Updates

--- assets/manageData.rdWETxKz.js
+++
@@ -1,1 +0,0 @@
-const c=()=>JSON.parse(localStorage.getItem("Accounts")||'[{"Platform":"Platform","Usage":0,"Accounts":[]}]'),s=t=>(localStorage.setItem("Accounts",JSON.stringify(t)),t);export{c as g,s};
