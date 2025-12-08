7bd1fb07013fc82ff5eedee90186c03d986c656d
Lifo123 <alberto_p-@hotmail.com>
2025-12-07 19:56:50 -0500

    Fix: typescript relative

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,7 @@
 import type { NextConfig } from "next";
-import path from "path";
 
 const nextConfig: NextConfig = {
   crossOrigin: 'anonymous',
-  turbopack: {
-    root: path.join(__dirname, '..'),
-  }
 
 };
 
